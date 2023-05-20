a23f9167064834b9336fae3528211ecf20e21bab
swag-likeohio <swag69ohio@gmail.com>
2023-05-20 18:44:59 +0200

    Initial commit

---
+++ HelloOhio/HelloOhio/Models/Lemezek.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetNuke.ComponentModel.DataAnnotations;
+using System.Web.Caching;
+using System.Web.UI.WebControls;
+
+namespace HelloOhio.HelloOhio.Models
+{
+    public class Lemezek
+    {
+        [TableName("Lemezek")]
+        [PrimaryKey(nameof(LemezID), AutoIncrement = true)]
+        [Cacheable("Lemezek", CacheItemPriority.Default, 20)]
+        [Scope("ModuleId")]
+        public class TestFlightPlan
+        {
+            public int LemezID { get; set; }
+
+            public string Cim { get; set; }
+
+            public string Eloado { get; set; }
+
+            public int Kiadaseve { get; set; }
+
+            public string Kiadoneve { get; set; }
+
+            public string Allapot { get; set; }
+
+        }
+    }
+}
